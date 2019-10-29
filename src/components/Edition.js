47409099816f62be515e48d6526175464962b8ce
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-10-29 18:28:45 +0100

    prepare html structure in Edition component

--- src/components/Edition.js
+++ src/components/Edition.js
@@ -3,10 +3,62 @@ import { Link } from 'react-router-dom';
 
 const Edition = (props) => {
   return(
-    <Link to='/detail/:dayList'>
-      <div>:(</div>
-    </Link>
-  )
+    <React.Fragment>
+      <form className="form">
+        <div className="date">
+          <label className="date__label" htmlFor="date">Fecha</label>
+          <input 
+            className="date__input"
+            type="date"
+            id="date"
+            name="date"
+            min="2019-01-01"
+            max="2019-31-12"
+            step="1"/>
+        </div>
+        <p className="state">Estado</p>
+        <div className="state__container">
+          <div>
+            <label className="state__happy__label" htmlFor="happy">
+              <input
+                classname="state__happy__input"
+                id="happy"
+                type="radio"
+                value=""
+                name="state"
+              />
+              :)
+            </label>
+          </div>
+          <div>
+            <label classname="state__sad__label" htmlFor="sad">
+              <input
+                classname="state__sad__input"
+                id="sad"
+                type="radio"
+                value=""
+                name="state"
+              />
+              :(
+            </label>
+          </div>
+        </div>
+        <div className="message">
+          <label className="message__label" htmlFor="message">Mensaje</label>
+            <input 
+              classname="message__input"
+              type="text"
+              id="message"
+              name="message"
+              />
+        </div>
+      </form>
+      <Link classname="edition__link" to='/detail/:dayList'>
+        <button classname="save__button" type="button">Guardar</button>
+        <button className="cancel__button" type="button">Cancelar</button>
+      </Link>
+    </React.Fragment>
+  );
 }
 
 export default Edition;
