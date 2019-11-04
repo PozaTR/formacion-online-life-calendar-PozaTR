5c6d39de90ae39b1d5f76f1c72a7cb3dc93732a6
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-11-04 19:15:47 +0100

    fix error to publish

---
+++ docs/precache-manifest.79beadd8108a5fe71abd3cec489574c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8721a38d828df22f9a8408f5d66dc27d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a111807c37783189de53",
+    "url": "./static/css/main.eb75e1a3.chunk.css"
+  },
+  {
+    "revision": "e58672f8a186a3653930",
+    "url": "./static/js/2.b99ba70c.chunk.js"
+  },
+  {
+    "revision": "a111807c37783189de53",
+    "url": "./static/js/main.c5414fff.chunk.js"
+  },
+  {
+    "revision": "6fa47f8e0ff788fa317e",
+    "url": "./static/js/runtime-main.68ec7a0d.js"
+  }
+]);
