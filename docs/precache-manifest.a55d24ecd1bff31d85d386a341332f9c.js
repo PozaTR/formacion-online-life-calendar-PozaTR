8ab66b81009d6364dd0d407e495463101b17c2b2
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-11-04 12:42:05 +0100

    create docs

---
+++ docs/precache-manifest.a55d24ecd1bff31d85d386a341332f9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea2aa04df3a84348c776173267359f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a111807c37783189de53",
+    "url": "/static/css/main.eb75e1a3.chunk.css"
+  },
+  {
+    "revision": "e58672f8a186a3653930",
+    "url": "/static/js/2.b99ba70c.chunk.js"
+  },
+  {
+    "revision": "a111807c37783189de53",
+    "url": "/static/js/main.c5414fff.chunk.js"
+  },
+  {
+    "revision": "367319d7d378c63b7311",
+    "url": "/static/js/runtime-main.ceb027f9.js"
+  }
+]);
